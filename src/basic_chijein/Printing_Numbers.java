19d2a4e7923be62a4733fa760a53ec0893ba89dd
partapkaran071-cyber <partapkaran071@gmail.com>
2026-08-04 20:54:25 +0530

    Add HelloKaran program in basics folder

---
+++ src/basic_chijein/Printing_Numbers.java
@@ -0,0 +1,12 @@
+package basic_chijein;
+
+public class Printing_Numbers {
+    public static void main(String[] args) {
+        int n;
+        n = 7;
+        System.out.println(n);
+
+        n = 50;
+        System.out.println(n);
+    }
+}
---
+++ src/basic_chijein/hellokarannnn.java
@@ -0,0 +1,10 @@
+package basic_chijein;
+
+public class hellokarannnn {
+    public static void main(String[] args){
+        System.out.println("hello I am karan");
+        System.out.println("I am learning java");
+        System.out.println("how are youuu\nI am going to be a coder");
+    }
+
+    }
