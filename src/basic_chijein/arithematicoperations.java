74aedb29d232ee0d6057bd201902477b889eb419
partapkaran071-cyber <partapkaran071@gmail.com>
2026-08-06 12:03:07 +0530

    Add basic Java practice programs (arithmetic, variables, problems 01–05)
    Add beginner Java exercises: circle area, sphere volume, cuboid TSA, square, simple interest
    
    Add HelloKaran and other introductory Java programs
    
    Add arithmetic operations and problem-solving examples in Java

---
+++ src/basic_chijein/arithematicoperations.java
@@ -0,0 +1,14 @@
+package basic_chijein;
+
+public class arithematicoperations {
+    public static void main(String[] args) {
+        double x = 35;
+        double y = 6;
+        System.out.println(x+y);
+        System.out.println(x-y);
+        System.out.println(x*y);
+        System.out.println(x/y);
+
+
+    }
+}
---
+++ src/basic_chijein/variables.java
@@ -0,0 +1,18 @@
+package basic_chijein;
+
+public class variables {
+    public static void main(String[] args) {
+        int x = 5;
+        System.out.println(x);
+        int y;//declaration
+        y = 10;//initilization
+        System.out.println(y);
+        System.out.println(x+y);
+
+        //modifying value of variables
+        //we know the value we given to  x= 5;
+        System.out.println(x);
+        x = x + 7;
+        System.out.println(x);
+    }
+}
