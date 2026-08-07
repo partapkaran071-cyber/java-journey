b2e1117f4d3c41c9179a0f21df0e76014beb9082
partapkaran071-cyber <partapkaran071@gmail.com>
2026-08-07 20:27:52 +0530

    Add char datatype program with ASCII and type casting
    add division data type

---
+++ src/basic_chijein/DivisionofDatatype.java
@@ -0,0 +1,9 @@
+package basic_chijein;
+
+public class DivisionofDatatype {
+   public static void main(String[] args) {
+       double x = 5.0/2;//use number with decimal places
+       System.out.println(x);
+
+    }
+}
