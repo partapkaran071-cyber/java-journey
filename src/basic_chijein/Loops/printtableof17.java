466321a9d29fb7c7d27015187158ca410f8e0ba6
partapkaran071-cyber <partapkaran071@gmail.com>
2026-09-14 20:04:23 +0530

    - Added printnumbernto1.java - Added printtableof17.java

---
+++ src/basic_chijein/Loops/printtableof17.java
@@ -0,0 +1,14 @@
+package basic_chijein.Loops;
+
+public class printtableof17 {
+     public static void main(String[] args) {
+        for(int i = 17; i<=170; i=i+17){
+             System.out.print(i+" ");
+
+        } //or
+         System.out.println();
+         for (int n = 1; n<=10; n++){
+             System.out.print(n*17+" ");
+         }
+    }
+}
