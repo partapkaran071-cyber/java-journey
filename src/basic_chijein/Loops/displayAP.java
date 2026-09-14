b97e777c0f7fcd690efdbcf6543182761f10bc9c
partapkaran071-cyber <partapkaran071@gmail.com>
2026-09-14 20:02:48 +0530

    feat: add basic if-else programs
    - Added divisibleby_5_and_3.java
    - Added fourdigitnumber.java
    - Added magnitudelessthan69.java
    - Added SidesofTriangle.java
    - Moved 'rc' folder to 'java_programs'
    - Added elseif.java
    - Added greatestof3no.java
    - Added lengthbreadthperimeterandareas.java
    - Added locationofapoint.java
    - Added print1to10.java
    - Added ternarybasics.java
    - Added ternaryinvariable.java
    - Added displayAP.java
    - Added oddnumberdivisbleby3.java

---
+++ src/basic_chijein/Loops/displayAP.java
@@ -0,0 +1,23 @@
+package basic_chijein.Loops;
+
+import java.util.Scanner;
+
+public class displayAP {
+   public static void main(String[] args) {
+       //Display this AP + 2,5,8,11....upto n term
+       Scanner sc = new Scanner(System.in);
+       System.out.println("Enter the number ");
+       int n = sc.nextInt();
+       for (int i = 2; i <= (3*n - 1); i+=3){
+           System.out.print(i+" ");
+
+       }
+       System.out.println();
+       //using loop without ap formula
+       int a = 2, d = 3;
+       for(int x = 1; x <= n; x++){
+           System.out.print(a+" ");
+           a+=d;
+       }
+    }
+}
