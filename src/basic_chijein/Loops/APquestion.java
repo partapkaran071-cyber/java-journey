1bc7af7856d705a6a6b27bbaea198271a514c095
partapkaran071-cyber <partapkaran071@gmail.com>
2026-09-15 10:22:05 +0530

    - Added Apquestion.java - Added compositenumbernadbreakstatement.java

---
+++ src/basic_chijein/Loops/APquestion.java
@@ -0,0 +1,11 @@
+package basic_chijein.Loops;
+
+public class APquestion {
+   public  static void main(String[] args) {
+       //Print AP 99, 95, 91,87............ upto all terms which are positive
+
+
+      for(int i = 99; i>0; i-=4) System.out.print(i+" ");
+
+    }
+}
