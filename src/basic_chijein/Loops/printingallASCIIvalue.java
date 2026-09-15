7353ebb119c36b181a4d07b440378156b7d22134
partapkaran071-cyber <partapkaran071@gmail.com>
2026-09-15 18:28:13 +0530

    - Added Gpquestion.java - Added printingallASCIIvalue.java

---
+++ src/basic_chijein/Loops/printingallASCIIvalue.java
@@ -0,0 +1,8 @@
+package basic_chijein.Loops;
+
+public class printingallASCIIvalue {
+    public static void main(String[] args) {
+        for(int i =65;  i <=90; i++)
+            System.out.println((char)i+" "+i);
+    }
+}
