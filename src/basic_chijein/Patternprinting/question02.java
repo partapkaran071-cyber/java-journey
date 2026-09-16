e7d86b22d6835f5b0586585aabdaaddcd666b3e5
partapkaran071-cyber <partapkaran071@gmail.com>
2026-09-16 09:36:39 +0530

    - Added question02.java - Added question03.java - Added question04.java

---
+++ src/basic_chijein/Patternprinting/question02.java
@@ -0,0 +1,17 @@
+package basic_chijein.Patternprinting;
+
+import java.util.Scanner;
+
+public class question02 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number");
+        int n = sc.nextByte();
+        for(int i =1; i<=n; i++){
+            for(int j =1; j<=n; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
