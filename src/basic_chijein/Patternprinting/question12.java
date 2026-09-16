7ac877eac06f48782b06b24815bc2f9211e13633
partapkaran071-cyber <partapkaran071@gmail.com>
2026-09-16 21:30:14 +0530

    - Added question12.java - Added question13.java

---
+++ src/basic_chijein/Patternprinting/question12.java
@@ -0,0 +1,21 @@
+package basic_chijein.Patternprinting;
+
+import java.util.Scanner;
+
+public class question12 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" "+" ");
+            }
+            for (int j = 1; j <= n; j++) {
+                System.out.print("*"+" ");
+            }
+            System.out.println();
+        }
+    }
+}
