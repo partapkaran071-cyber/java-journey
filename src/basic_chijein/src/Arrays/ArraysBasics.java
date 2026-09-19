d907beb6d3aa13046579541c48427f7e7789d02d
partapkaran071-cyber <partapkaran071@gmail.com>
2026-09-19 19:22:23 +0530

    Resolve merge conflict in ArraysBasics.java

--- src/basic_chijein/src/Arrays/ArraysBasics.java
+++ src/basic_chijein/src/Arrays/ArraysBasics.java
@@ -1,4 +1,23 @@
 package basic_chijein.src.Arrays;
 
 public class ArraysBasics {
+    public static void main(String[] args) {
+        int[] x = {6,19,50,30,17,18,171};
+
+        //indexing (1)
+        System.out.println(x[5]);
+        //updating elements - mutability
+        x[3]=15;
+        System.out.println(x[3]);
+
+        int[] arr = new int[4]; // 4 size element from 0 to 3
+        arr[0]=10;
+        arr[1]=11;
+        arr[2]=12;
+        arr[3]=13;
+        System.out.println(arr[0]);
+        System.out.println(arr[1]);
+        System.out.println(arr[2]);
+        System.out.println(arr[3]);
+    }
 }
