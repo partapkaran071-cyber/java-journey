51a986672e8ed54e49b5932c4b5bc21626e04a29
partapkaran071-cyber <partapkaran071@gmail.com>
2026-09-19 18:21:59 +0530

    Resolve merge conflicts and preserve lecture files

--- src/basic_chijein/src/Firstprogram/hellokarannnn.java
+++ src/basic_chijein/src/Firstprogram/hellokarannnn.java
@@ -1,14 +1,9 @@
-<<<<<<<< HEAD:src/basic_chijein/src/basic_programs/hellokarannnn.java
-package basic_chijein.src.basic_programs;
-========
-package basic_chijein.Basicprogrammes;
->>>>>>>> lecture-files:src/basic_chijein/Basicprogrammes/hellokarannnn.java
+package basic_chijein.src.Firstprogram;
 
 public class hellokarannnn {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("hello I am karan");
         System.out.println("I am learning java");
         System.out.println("how are youuu\nI am going to be a coder");
     }
-
-    }
+}
