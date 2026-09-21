cccd8d10b9079f420063ab25194bb7a24433b4fa
partapkaran071-cyber <partapkaran071@gmail.com>
2026-09-21 23:08:11 +0530

    -modified Twosums.java

--- src/basic_chijein/src/Arrays/Twosums.java
+++ src/basic_chijein/src/Arrays/Twosums.java
@@ -2,8 +2,19 @@ package Arrays;
 
 public class Twosums {
     public static void main(String[] args) {
+                int[] nums = {2, 7, 11, 15};
+                int target = 9;
 
-  boolean twosum(int arr[], int target){
-      int n =
+                // Check all pairs
+                for (int i = 0; i < nums.length; i++) {
+                    for (int j = i + 1; j < nums.length; j++) {
+                        if (nums[i] + nums[j] == target) {
+                            System.out.println("Indices: " + i + ", " + j);
+                            System.out.println("Numbers: " + nums[i] + ", " + nums[j]);
+                        }
+                    }
+                }
+            }
         }
-}
+
+
